e7c4d61c2a5a355432576b26c7ea1fd880ae2612
DucHungGithub <100983291+DucHungGithub@users.noreply.github.com>
2023-08-25 22:08:10 +0700

    1:32:00

--- lib/controllers/question_page/data_uploader.dart
+++ lib/controllers/question_page/data_uploader.dart
@@ -1,30 +1,60 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:study_app_project/firebase_ref/references.dart';
 
 import '../../models/question_paper_model.dart';
 
-class DataUploader extends GetxController{
+class DataUploader extends GetxController {
   @override
-  void onReady(){
+  void onReady() {
     uploadData();
     super.onReady();
   }
 
   Future<void> uploadData() async {
-    final manifestContent = await DefaultAssetBundle.of(Get.context!).loadString("AssetManifest.json");
-    final Map<String,dynamic> manifestMap = json.decode(manifestContent);
+    final fireStore = FirebaseFirestore.instance;
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
     //load json file and print path
-    final papersInAssets = manifestMap.keys.where((path) => path.startsWith("assets/DB/paper")&& path.contains(".json")).toList();
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/paper") && path.contains(".json"))
+        .toList();
     List<QuestionPaperModel> questionPapers = [];
-    for(var paper in papersInAssets){
+    for (var paper in papersInAssets) {
       String stringPaperContent = await rootBundle.loadString(paper);
-      questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
     }
-    print('Item number ${questionPapers.length}');
+    //print('Item number ${questionPapers[0].description}');
+    var batch = fireStore.batch();
 
+    for (var paper in questionPapers) {
+      batch.set(questionPaperRF.doc(paper.id), {
+        "title": paper.title,
+        "image_url": paper.imageUrl,
+        "description": paper.description,
+        "time_seconds": paper.timeSeconds,
+        "questions_count": paper.questions == null ? 0 : paper.questions!.length
+      });
+      for (var questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          "question": questions.question,
+          "correct_answer": questions.correctAnswer
+        });
+        for (var answer in questions.answers) {
+          batch.set(questionPath.collection("answers").doc(answer.identifier),
+              {"identifier": answer.identifier, "Answer": answer.answer});
+        }
+      }
+    }
+    await batch.commit();
   }
-
 }
