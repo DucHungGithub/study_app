6704de65406cb57946666f7ed11f2a3ff826003f
phucst123 <huuphucst123@gmail.com>
2023-09-03 11:28:19 +0700

    modify correspond to controller

--- lib/screen/question/questions_screen.dart
+++ lib/screen/question/questions_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:study_app_project/controllers/question_page/questions_controller.dart';
 import 'package:study_app_project/screen/home/question_card.dart';
 import 'package:study_app_project/widgets/common/background_decoration.dart';
 
-class QuestionsScreen extends StatelessWidget {
+class QuestionsScreen extends GetView<QuestionsController> {
   const QuestionsScreen({super.key});
 
   static const String routeName = "/questionsscreen";
