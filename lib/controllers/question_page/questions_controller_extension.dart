dc9283ee002150fa8b0cb81fcd6d4f8d658cf63b
phucst123 <huuphucst123@gmail.com>
2023-09-15 07:09:35 +0700

    fix spelling error

--- lib/controllers/question_page/questions_controller_extension.dart
+++ lib/controllers/question_page/questions_controller_extension.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:study_app_project/controllers/auth_controller.dart';
 import 'package:study_app_project/controllers/question_page/questions_controller.dart';
 import 'package:study_app_project/firebase_ref/references.dart';
+import 'package:study_app_project/models/datetime.dart';
 
 extension QuestionsControllerExtension on QuestionsController {
   int get correctQuestionCount => allQuestions
@@ -22,6 +23,26 @@ extension QuestionsControllerExtension on QuestionsController {
     return points.toStringAsFixed(2);
   }
 
+  // Future<void> saveTestResults() async {
+  //   var batch = fireStore.batch();
+  //   User? user = Get.find<AuthController>().getUser();
+  //   if (user == null) {
+  //     return;
+  //   }
+  //   batch.set(
+  //       userRF
+  //           .doc(user.email)
+  //           .collection('myRecent_tests')
+  //           .doc(questionPaperModel.id),
+  //       {
+  //         "points": points,
+  //         "correct_answer": '$correctQuestionCount / ${allQuestions.length}',
+  //         "question_id": questionPaperModel.id,
+  //         "time": questionPaperModel.timeSeconds - remainSeconds
+  //       });
+  //   batch.commit();
+  //   navigateToHome();
+  // }
   Future<void> saveTestResults() async {
     var batch = fireStore.batch();
     User? user = Get.find<AuthController>().getUser();
@@ -29,16 +50,17 @@ extension QuestionsControllerExtension on QuestionsController {
       return;
     }
     batch.set(
-        userRF
-            .doc(user.email)
-            .collection('myRecent_tests')
-            .doc(questionPaperModel.id),
-        {
-          "points": points,
-          "correct_answer": '$correctQuestionCount / ${allQuestions.length}',
-          "question_id": questionPaperModel.id,
-          "time": questionPaperModel.timeSeconds - remainSeconds
-        });
+      userRF.doc(user.email).collection(questionPaperModel.id).doc(),
+      {
+        "point": points,
+        "correct_question": correctQuestionCount,
+        "total_question:": allQuestions.length,
+        "question_id": questionPaperModel.id,
+        "title": questionPaperModel.title,
+        "time": questionPaperModel.timeSeconds - remainSeconds,
+        "date": todaysDateFormatted(),
+      },
+    );
     batch.commit();
     navigateToHome();
   }
